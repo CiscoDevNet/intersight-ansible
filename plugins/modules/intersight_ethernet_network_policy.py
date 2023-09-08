8cde613522bedaabae03389adc9134db64d64e8e
Surendra Ramarao <chsurend@cisco.com>
2023-09-08 13:23:13 +0530

    ethernet network

--- plugins/modules/intersight_ethernet_network_policy.py
+++ plugins/modules/intersight_ethernet_network_policy.py
@@ -12,11 +12,11 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 
 DOCUMENTATION = r'''
 ---
-module: intersight_ethernet_qos_policy
-short_description: Ethernet Qos policy configuration for Cisco Intersight
+module: intersight_ethernet_network_policy
+short_description: Ethernet Network Policy configuration for Cisco Intersight
 description:
-  - Ethernet Qos policy configuration for Cisco Intersight.
-  - Used to configure Ethernet Qos Policy on Cisco Intersight managed devices.
+  - Ethernet Network Policy configuration for Cisco Intersight.
+  - Used to configure Ethernet Network Policy on Cisco Intersight managed devices.
   - For more information see L(Cisco Intersight,https://intersight.com/apidocs).
 extends_documentation_fragment: intersight
 options:
@@ -50,66 +50,52 @@ options:
       - Description can contain letters(a-z, A-Z), numbers(0-9), hyphen(-), period(.), colon(:), or an underscore(_).
     aliases: [descr]
     type: str
-  burst:
+  vlan_settings:
     description:
-      -  The burst traffic, in bytes, allowed on the vNIC.
-    default: 10240
-    type: int
-  cos:
-    description:
-      -  Class of Service to be associated to the traffic on the virtual interface.
-    default: 0
-    type: int
-  mtu:
-    description:
-      -  The Maximum Transmission Unit (MTU) or packet size that the virtual interface accepts.
-    default: 1500
-    type: int
-  priority:
-    description:
-      -  The priority matching the System QoS specified in the fabric profile.
-      -  Best Effort - QoS priority for Best-effort traffic.
-      -  FC - QoS priority for FC traffic.
-      -  Platinum - QoS priority for Platinum traffic.
-      -  Gold - QoS priority for Gold traffic.
-      -  Silver - QoS priority for Silver traffic.
-      -  Bronze - QoS priority for Bronze traffic.
-    choices: ['Best Effort' , 'FC' , 'Platinum' , 'Gold' , 'Silver' , 'Bronze']
-    default: Best Effort
-    type: str
-  rate_limit:
-    description:
-      -  The value in Mbps (0-10G/40G/100G depending on Adapter Model) to use for limiting the data rate on the virtual interface.
-    default: 0
-    type: int
-  trust_host_cos:
-    description:
-      -  Enables usage of the Class of Service provided by the operating system.
-    default: False
-    type: bool
+      -  VLAN configuration for the virtual interface.
+    type: list
+    elements: dict
+    suboptions:
+      default_vlan:
+        description:
+          - Native VLAN ID of the virtual interface or the corresponding vethernet on the peer Fabric Interconnect to which the virtual interface is connected.
+          - Setting the ID to 0 will not associate any native VLAN to the traffic on the virtual interface.
+        type: int
+        default: 0
+      mode:
+        description:
+          - Option to determine if the port can carry single VLAN (Access) or multiple VLANs (Trunk) traffic.
+          - ACCESS - An access port carries traffic only for a single VLAN on the interface.
+          - TRUNK - A trunk port can have two or more VLANs configured on the interface. It can carry traffic for several VLANs simultaneously.
+        type: str
+        choices: ['ACCESS', 'TRUNK']
+        default: ACCESS
 author:
   - Surendra Ramarao (@CRSurendra)
 '''
 
 EXAMPLES = r'''
-- name: Configure Ethernet QoS Policy
-  cisco.intersight.intersight_ethernet_qos_policy:
+- name: Configure Ethernet Network Policy
+  cisco.intersight.intersight_ethernet_network_policy:
     api_private_key: "{{ api_private_key }}"
     api_key_id: "{{ api_key_id }}"
     organization: DevNet
-    name: COS-EQOS
-    description: Ethernet Qos policy for COS
+    name: COS-ENWP
+    description: Ethernet Network Policy for COS
     tags:
       - Key: Site
         Value: RCDN
-    mtu: 1500
+    vlan_settings:
+      - default_vlan: 693
+        mode: ACCESS
 
-- name: Delete Ethernet Qos Policy
+
+- name: Delete Ethernet Ethernent Network Policy
   cisco.intersight.intersight_ethernet_qos_policy:
     api_private_key: "{{ api_private_key }}"
     api_key_id: "{{ api_key_id }}"
     organization: DevNet
-    name: COS-EQOS
+    name: COS-ENWP
     state: absent
 '''
 
@@ -120,8 +106,8 @@ api_repsonse:
   type: dict
   sample:
     "api_response": {
-        "Name": "COS-LCP",
-        "ObjectType": "vnic.EthQosPolicy",
+        "Name": "COS-ENWP",
+        "ObjectType": "vnic.EthNetworkPolicy",
         "Tags": [
             {
                 "Key": "Site",
@@ -136,12 +122,24 @@ from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.cisco.intersight.plugins.module_utils.intersight import IntersightModule, intersight_argument_spec
 
 
-def check_and_add_prop(prop, propKey, params, api_body):
-    if propKey in params.keys():
-        api_body[prop] = params[propKey]
+def check_and_add_prop_dict(prop, prop_key, params, api_body):
+    if prop_key in params.keys():
+        api_body[prop] = {}
+        if params[prop_key] :
+            for item in params[prop_key]:
+                for key in item.keys():
+                    api_body[prop][to_camel_case(key)] = item[key]
+
+
+def to_camel_case(snake_str):
+    return "".join(x.capitalize() for x in snake_str.lower().split("_"))
 
 
 def main():
+    vlan_settings_spec = {
+        "default_vlan" : {"type": "int", "default": 0},
+        "mode": {"type": "str", "default": "ACCESS", "choices": ['ACCESS', 'TRUNK']},
+    }
     argument_spec = intersight_argument_spec
     argument_spec.update(
         state={"type": "str", "choices": ['present', 'absent'], "default": "present"},
@@ -149,37 +147,10 @@ def main():
         name={"type": "str", "required": True},
         description={"type": "str", "aliases": ['descr']},
         tags={"type": "list", "elements": "dict"},
-        burst={
-            "type": "int",
-            "default": 10240
-        },
-        cos={
-            "type": "int",
-            "default": 0
-        },
-        mtu={
-            "type": "int",
-            "default": 1500
-        },
-        priority={
-            "type": "str",
-            "choices": [
-                'Best Effort',
-                'FC',
-                'Platinum',
-                'Gold',
-                'Silver',
-                'Bronze'
-            ],
-            "default": "Best Effort"
-        },
-        rate_limit={
-            "type": "int",
-            "default": 0
-        },
-        trust_host_cos={
-            "type": "bool",
-            "default": False
+        vlan_settings={
+            "type": "list",
+            "elements": "dict",
+            "options": vlan_settings_spec
         },
     )
 
@@ -195,7 +166,7 @@ def main():
     # Argument spec above, resource path, and API body should be the only code changed in each policy module
     #
     # Resource path used to configure policy
-    resource_path = '/vnic/EthQosPolicies'
+    resource_path = '/vnic/EthNetworkPolicies'
     # Define API body used in compares or create
     intersight.api_body = {
         'Organization': {
@@ -205,12 +176,7 @@ def main():
         'Tags': intersight.module.params['tags'],
         'Description': intersight.module.params['description'],
     }
-    check_and_add_prop('Burst', 'burst', intersight.module.params, intersight.api_body)
-    check_and_add_prop('Cos', 'cos', intersight.module.params, intersight.api_body)
-    check_and_add_prop('Mtu', 'mtu', intersight.module.params, intersight.api_body)
-    check_and_add_prop('Priority', 'priority', intersight.module.params, intersight.api_body)
-    check_and_add_prop('RateLimit', 'rate_limit', intersight.module.params, intersight.api_body)
-    check_and_add_prop('TrustHostCos', 'trust_host_cos', intersight.module.params, intersight.api_body)
+    check_and_add_prop_dict('VlanSettings', 'vlan_settings', intersight.module.params, intersight.api_body)
     #
     # Code below should be common across all policy modules
     #
