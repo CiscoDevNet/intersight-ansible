fefd6cc8160e5aafb6309b656f01f495d6ff6331
“dsoper2” <dsoper@cisco.com>
2025-10-23 13:06:55 -0500

    bash script for pre-ansible-run

---
+++ playbooks/command.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if at least one argument is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <command> [args...]"
+    exit 1
+fi
+
+# Run the command with all arguments
+"$@"
