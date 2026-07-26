fb865c679f091fcd1a8c3b9b5fe38f520b65147f
Ron Gershburg <rgershbu@redhat.com>
2026-07-26 09:07:06 +0300

    chore: bump dummy comment for ansible-test ci retest

--- plugins/modules/intersight_ntp_policy_info.py
+++ plugins/modules/intersight_ntp_policy_info.py
@@ -3,7 +3,7 @@
 
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-# Dummy change to exercise ansible-test CI change detection.
+# Dummy change to exercise ansible-test CI change detection (round 2).
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
